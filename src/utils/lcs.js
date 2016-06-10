ef15e725d50437c721588839354cce2189ca3882
Eyal Arubas <eyalarubas@gmail.com>
2016-06-10 13:59:50 +0100

    implement arrays lcs

---
+++ src/utils/lcs.js
@@ -0,0 +1,54 @@
+import { maxBy } from "lodash";
+
+class Memory {
+  constructor(n, m) {
+    this.m = m;
+    this.mem = new Array(n * m);
+  }
+
+  _toIndex(i, j) {
+    return this.m * i + j;
+  }
+
+  get(i, j) {
+    return this.mem[this._toIndex(i, j)];
+  }
+
+  set(i, j, val) {
+    return this.mem[this._toIndex(i, j)] = val;
+  }
+
+  getOrSet(i, j, setter) {
+    return (
+      this.get(i, j) ||
+      this.set(i, j, setter())
+    );
+  }
+}
+
+const longest = (a1, a2) => maxBy([a1, a2], a => a.length);
+
+const lcs = (a1, a2, mem) => {
+  mem = mem || new Memory(a1.length - 1, a2.length - 1);
+  // length-1 because we never use (0,0).
+  if (!a1.length || !a2.length) return [];
+  if (a1[a1.length - 1] === a2[a2.length - 1]) return mem.getOrSet(
+    a1.length - 1,
+    a2.length - 1,
+    () => lcs(a1.slice(0, -1), a2.slice(0, -1), mem)
+  ).concat(a1[a1.length - 1]);
+  return longest(
+    mem.getOrSet(
+      a1.length - 1,
+      a2.length,
+      () => lcs(a1.slice(0, -1), a2, mem)
+    ),
+    mem.getOrSet(
+      a1.length,
+      a2.length - 1,
+      () => lcs(a1, a2.slice(0, -1), mem)
+    )
+  );
+};
+
+export default lcs;
