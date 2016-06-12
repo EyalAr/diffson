1f4167399a0775d2781fdd55deda05c527b67c22
Eyal Arubas <eyalarubas@gmail.com>
2016-06-12 13:35:52 +0100

    add code documentation

--- src/calcs/arrays.js
+++ src/calcs/arrays.js
@@ -6,35 +6,50 @@ import ofSameComplexType from "../utils/ofSameComplexType";
 
 export default (base, target) => {
   if (!isArray(base) || !isArray(target)) return false;
-  const common = lcs(base, target),
-        deletions = difference(base, common),
-        additions = difference(target, common),
-        commonExp = expandPartialArray(base, common),
-        deletionsExp = expandPartialArray(base, deletions),
-        additionsExp = expandPartialArray(target, additions),
-        res = [];
-  var i = 0, j = 0;
+  const res = [];
+  var common = lcs(base, target),
+      // 'common' holds fixed elements which are both in base and target
+      deletions = difference(base, common),
+      // 'deletions' holds elements which are in 'base' but not in 'target'.
+      additions = difference(target, common);
+      // 'additions' holds elements which are in 'target' but not in 'base'.
+  // all of the above arrays are subsequences of a larger array.
+  // expand these arrays to be of the same size as the larger array,
+  // by making them sparse arrays with gaps.
+  common = expandPartialArray(base, common);
+  deletions = expandPartialArray(base, deletions);
+  additions = expandPartialArray(target, additions);
+  var i = 0, // 'base' iterator
+      j = 0; // 'target' iterator
   for (; i < base.length; i++) {
-    if (i in commonExp) {
-      res.push(generators.noop(commonExp[i], commonExp[i]));
+    // for each element in the base array, we need to determine what happens
+    // to it in the target array.
+    if (i in common) {
+      // this element exists both in base and in target.
+      res.push(generators.noop(common[i], common[i]));
       j++;
-    } else if (j in additionsExp && i in deletionsExp) {
-      if (ofSameComplexType(deletionsExp[i], additionsExp[j])) {
-        res.push(generators.recurse(deletionsExp[i], additionsExp[j]));
+    } else if (i in deletions && j in additions) {
+      // this element was removed in 'base', but another was added in 'target'
+      // at the same positions
+      if (ofSameComplexType(deletions[i], additions[j])) {
+        res.push(generators.recurse(deletions[i], additions[j]));
       } else {
-        res.push(generators.replace(deletionsExp[i], additionsExp[j]));
+        res.push(generators.replace(deletions[i], additions[j]));
       }
       j++;
-    } else if (j in additionsExp) {
-      res.push(generators.add(undefined, additionsExp[j]));
+    } else if (j in additions) {
+      res.push(generators.add(undefined, additions[j]));
       j++;
-    } else if (i in deletionsExp) {
-      res.push(generators.remove(deletionsExp[i], undefined));
+    } else if (i in deletions) {
+      res.push(generators.remove(deletions[i], undefined));
     }
   }
+  // if there are still elements remaining in 'target' which we didn't account
+  // for, they must be additions:
   for (; j < target.length; j++) {
-    res.push(generators.add(undefined, additionsExp[j]));
+    res.push(generators.add(undefined, additions[j]));
   }
+
   return res.map((r, i) => {
     r.path = new Array(res.length);
     r.path[i] = {};
