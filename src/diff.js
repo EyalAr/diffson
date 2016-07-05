118bc6ccc636848201c68824ef6b58ee7a4841bb
Eyal Arubas <eyalarubas@gmail.com>
2016-07-05 10:42:58 +0100

    support for maximal diff depth

--- src/diff.js
+++ src/diff.js
@@ -7,20 +7,22 @@ const pipeline = {
   "arrays": "init"
 };
 
-const diff = (base, target, path = {}, calc = "init") => {
+const diff = (base, target, depth = -1, path = {}, calc = "init") => {
   const deltas = calcs[calc](base, target);
   if (!deltas) return false;
   return deltas.reduce((res, delta) => {
-    if (delta.action === "recurse") {
+    if (delta.action === "recurse" && depth) {
       var deltas;
       while (!(deltas = diff(
         delta.details.from,
         delta.details.to,
+        Math.max(-1, depth - 1),
         delta.path,
         calc = pipeline[calc]
       )));
       res.push.apply(res, deltas);
     } else {
+      if (delta.action === "recurse") delta.action = "replace";
       res.push(delta);
     }
     return res;
